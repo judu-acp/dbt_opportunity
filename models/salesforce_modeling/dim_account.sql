63b58ce37ca57e6e72f4bb9f1648cf81dbd8c5ac
joshuduacp@gmail.com <joshuduacp@gmail.com>
2022-04-26 23:41:39 +0000

    added marts and began adding models/macros for dim_date

---
+++ models/salesforce_modeling/dim_account.sql
@@ -0,0 +1,5 @@
+SELECT 
+        id                                                                      AS dim_acc_id,
+        parent_id                                                               AS parent_acc_id,
+        name                                                                    AS acc_name
+    FROM analytics.amplify_salesforce.account
--- models/salesforce_modeling/fct_opportunity.sql
+++ models/salesforce_modeling/fct_opportunity.sql
@@ -1,5 +1,10 @@
+{{ config(
+    materialized = "table"
+) }}
+
 SELECT
     opportunity.id                                                          AS dim_opp_id,
+    to_char(close_date, 'YYYYMMDD')::int                                    AS dim_close_date_id,
     account_id                                                              AS dim_acc_id,
     consultant_c.id                                                         AS dim_consultant_id,
     owner_id                                                                AS dim_acc_manager_id,
