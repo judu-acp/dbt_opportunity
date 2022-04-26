d8e4f2a0a680debe8362ad9794bba44cf8e21f6c
joshuduacp@gmail.com <joshuduacp@gmail.com>
2022-04-26 23:54:04 +0000

    removed incomplete date files

--- macros/datetime/date_spine.sql
+++
@@ -1,3 +0,0 @@
-{% macro date_spine(datepart, start_date, end_date) %}
-    {{ return(adapter.dispatch('date_spine', packages = dbt_utils._get_utils_namespaces())(datepart, start_date, end_date)) }}
-{%- endmacro %}
--- models/salesforce_modeling/date_details.sql
+++
@@ -1,9 +0,0 @@
-WITH source AS (
-
-    SELECT *
-    FROM {{ ref('date_details_source')}}
-
-)
-
-SELECT *
-FROM source
