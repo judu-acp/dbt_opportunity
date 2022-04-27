02dc493c60379d2af5c420ac0959eabff10f5423
joshuduacp@gmail.com <joshuduacp@gmail.com>
2022-04-27 00:15:37 +0000

    materialize as tables, renamed fct_opportunity

--- models/salesforce_modeling/dim_account.sql
+++ models/salesforce_modeling/dim_account.sql
@@ -1,3 +1,7 @@
+{{ config(
+    materialized = "table"
+) }}
+
 SELECT 
         id                                                                      AS dim_acc_id,
         parent_id                                                               AS parent_acc_id,
